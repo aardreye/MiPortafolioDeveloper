e0cda256dd5c1f06eeeb5f8a0c4a4a6d63b7df04
aardreye <aardreye@espol.edu.ec>
2022-08-19 02:56:28 -0500

    login

--- Proyecto3-MiPeluqueria/mipeluqueriaAngular/README.md
+++ Proyecto3-MiPeluqueria/mipeluqueriaAngular/README.md
@@ -1,4 +1,4 @@
-# MipeluqueriaNG
+# MipeluqueriaAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.1.
 
--- Proyecto3-MiPeluqueria/mipeluqueriaAngular/src/app/app.component.spec.ts
+++ Proyecto3-MiPeluqueria/mipeluqueriaAngular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mipeluqueriaNG'`, () => {
+  it(`should have as title 'mipeluqueriaAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mipeluqueriaNG');
+    expect(app.title).toEqual('mipeluqueriaAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mipeluqueriaNG app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mipeluqueriaAngular app is running!');
   });
 });
